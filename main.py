d8945d6abac7c591e91367a1940823a7bb972a7f
Jesse Palmer <jesselpalmer@gmail.com>
2017-06-24 23:42:12 -0700

    feat: add module

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Module contains helper methods to get financial data
+"""
+import urllib2
+import json
+from bs4 import BeautifulSoup
+
+GOOGLE_FINANCE_URL = 'https://www.google.com/finance/info?q='
+
+def get_stock_price(stock):
+    """
+    Gets stock price
+    """
+    page = urllib2.urlopen(GOOGLE_FINANCE_URL + stock)
+    soup = BeautifulSoup(page, "lxml")
+    response_string = soup.body.string
+    stock_string = response_string[2:]
+    stock_string_length = len(stock_string)
+    stock_object = stock_string[2:stock_string_length - 2]
+    stock_json = json.loads(stock_object)
+    print stock_json['l']
+
+get_stock_price('GOOG')
